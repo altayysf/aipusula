b47a72350a10d6ff465ea0c9ee07f63250fd4eb8
altay <ysfaltay25@gmail.com>
2026-02-01 20:01:18 +0300

    adsense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,6 +65,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>      
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6397577664152215"
+      crossOrigin="anonymous"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
